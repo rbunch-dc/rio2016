a82c02c82999f94c688d7fb904401954cca7b961
sikB <sikhovski@icloud.com>
2016-04-12 10:15:27 -0400

    finished the first part of the page--side nav bar, header, symbol, countdown

--- libs/countdown/assets/js/script.js
+++ libs/countdown/assets/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
 	
 	var note = $('#note'),
 		ts = new Date(2012, 0, 1),
@@ -10,7 +10,8 @@ $(function(){
 		ts = (new Date()).getTime() + 10*24*60*60*1000;
 		newYear = false;
 	}
-		
+
+
 	$('#countdown').countdown({
 		timestamp	: ts,
 		callback	: function(days, hours, minutes, seconds){
