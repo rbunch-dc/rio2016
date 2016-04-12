f374012d3e65768e8c140e6d54f5a2df4d93d049
sikB <sikhovski@icloud.com>
2016-04-12 10:44:18 -0400

    fixed the countdown to the correct date for Rio 2016

--- assets/js/temp.js
+++ assets/js/temp.js
@@ -130,38 +130,38 @@
 });
 
 $(function(){
-  
-  var note = $('#note'),
-    ts = new Date(2012, 0, 1),
-    newYear = true;
-  
-  if((new Date()) > ts){
-    // The new year is here! Count towards something else.
-    // Notice the *1000 at the end - time must be in milliseconds
-    ts = (new Date()).getTime() + 10*24*60*60*1000;
-    newYear = false;
-  };
-    
-  $('#countdown').countdown({
-    timestamp : ts,
-    callback  : function(days, hours, minutes, seconds){
-      
-      var message = "";
-      
-      message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-      message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-      message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-      message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-      
-      if(newYear){
-        message += "left until the new year!";
-      }
-      else {
-        message += "left to 10 days from now!";
-      }
-      
-      note.html(message);
-    }
-  });
-  
+	
+	var note = $('#note'),
+		ts = new Date(2016, 7, 5),
+		newYear = true;
+	
+	if((new Date()) > ts){
+		// The new year is here! Count towards something else.
+		// Notice the *1000 at the end - time must be in milliseconds
+		ts = (new Date()).getTime() + 10*24*60*60*1000;
+		newYear = false;
+	}
+		
+	$('#countdown').countdown({
+		timestamp	: ts,
+		callback	: function(days, hours, minutes, seconds){
+			
+			var message = "";
+			
+			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+			
+			if(newYear){
+				message += "left until Rio 2016!";
+			}
+			else {
+				message += "left to Rio 2016!";
+			}
+			
+			note.html(message);
+		}
+	});
+	
 });
